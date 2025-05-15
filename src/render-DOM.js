7c72a811a0f080c8877c1093d50f85d4c90908bf
James Forino <57734046+jforino@users.noreply.github.com>
2025-05-15 14:19:26 -0700

    create forecast function childDiv

--- src/render-DOM.js
+++ src/render-DOM.js
@@ -1,10 +1,22 @@
 const cityNameDivHolder = document.getElementById('city-name'); 
 const currentTempHolder = document.getElementById('current-temp'); 
 const currentConditions = document.getElementById('current-conditions'); 
+const forecastDivContainer = document.getElementById('forecast-div-container'); 
 
 export function insertWeatherData(APICallObject){
     cityNameDivHolder.textContent = `City: ${APICallObject.resolvedAddress}`
     currentTempHolder.textContent = `Current Temp: ${APICallObject.currentConditions.temp}`
     currentConditions.textContent = `Current Conditions: ${APICallObject.currentConditions.conditions}`
+
+    let forecastArray = APICallObject.days; 
+
+    for(let i = 1; i < 4; i++){
+        let currentForecastDay = forecastArray[i]; 
+        console.log(currentForecastDay);
+
+
+    }
+
+    console.log(forecastArray); 
 
 }
